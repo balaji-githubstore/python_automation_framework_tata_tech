299972f42a1d63273e1abf24b24a558ccef4bee9
balaji-githubstore <balaji0017@gmail.com>
2022-12-22 10:33:08 +0530

    Create Two Test Methods Using pytest

---
+++ tests/test_login.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from assertpy import assert_that
+
+
+class TestLoginUI:
+
+    def test_title(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(30)
+        self.driver.get("https://demo.openemr.io/b/openemr")
+        assert_that("OpenEMR Login").is_equal_to(self.driver.title)
+
+    def test_app_desc(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(30)
+        self.driver.get("https://demo.openemr.io/b/openemr")
+        actual_desc = self.driver.find_element(By.XPATH, "//p[contains(text(),'most')]").text
+        assert_that(actual_desc).contains("Electronic Health Record and Medical Practice Management")
