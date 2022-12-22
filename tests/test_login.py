e3337afea7ad25ea3265dcb99fb9d0b00d643d9c
balaji-githubstore <balaji0017@gmail.com>
2022-12-22 10:34:51 +0530

    Added Fixture with scope as Function and autouse as true

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,18 +5,18 @@ from assertpy import assert_that
 
 
 class TestLoginUI:
-
-    def test_title(self):
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.implicitly_wait(30)
         self.driver.get("https://demo.openemr.io/b/openemr")
+        yield
+        self.driver.quit()
+
+    def test_title(self):
         assert_that("OpenEMR Login").is_equal_to(self.driver.title)
 
     def test_app_desc(self):
-        self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
-        self.driver.implicitly_wait(30)
-        self.driver.get("https://demo.openemr.io/b/openemr")
         actual_desc = self.driver.find_element(By.XPATH, "//p[contains(text(),'most')]").text
         assert_that(actual_desc).contains("Electronic Health Record and Medical Practice Management")
