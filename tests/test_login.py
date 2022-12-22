82eb4b97d639f13eed99ccf08e2dffdef3da351a
balaji-githubstore <balaji0017@gmail.com>
2022-12-22 13:46:06 +0530

    Taking data from data_source.py for @pytest.mark.parametrize

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,29 +5,8 @@ from assertpy import assert_that
 from selenium.webdriver.support.select import Select
 
 from base.webdriver_listener import WebDriverWrapper
+from utilities import data_source
 
-
-class TestLogin(WebDriverWrapper):
-
-    @pytest.mark.parametrize("username,password,language,expected_title",
-                             [
-                                 ("admin", "pass", "English (Indian)", "OpenEMR"),
-                                 ("physician", "physician", "English (Indian)", "OpenEMR"),
-                                 ("accountant", "accountant", "English (Indian)", "OpenEMR")
-                             ]
-                             )
-    def test_valid_login(self, username, password, language, expected_title):
-        self.driver.find_element(By.ID, "authUser").send_keys(username)
-        self.driver.find_element(By.CSS_SELECTOR, "#clearPass").send_keys(password)
-        select_lan = Select(self.driver.find_element(By.XPATH, "//select[@name='languageChoice']"))
-        select_lan.select_by_visible_text(language)
-        self.driver.find_element(By.ID, "login-button").click()
-        assert_that(expected_title).is_equal_to(self.driver.title)
-        #assert_that("Week").is_equal_to(self.driver.find_element(By.XPATH,"").text)
-
-        assert self.driver.find_element(By.ID,"username").is_displayed()
-        assert_that(self.driver.find_element(By.ID,"username").is_displayed()).is_true()
-        assert_that(self.driver.page_source).does_not_contain("67766767")
 
 class TestLoginUI(WebDriverWrapper):
     def test_title(self):
@@ -44,3 +23,23 @@ class TestLoginUI(WebDriverWrapper):
 
         assert_that("Username").is_equal_to(actual_username_placeholder)
         assert_that("Password").is_equal_to(actual_password_placeholder)
+
+
+class TestLogin(WebDriverWrapper):
+
+    @pytest.mark.parametrize("username,password,language,expected_title", data_source.test_valid_login_data)
+    def test_valid_login(self, username, password, language, expected_title):
+        self.driver.find_element(By.ID, "authUser").send_keys(username)
+        self.driver.find_element(By.CSS_SELECTOR, "#clearPass").send_keys(password)
+        select_lan = Select(self.driver.find_element(By.XPATH, "//select[@name='languageChoice']"))
+        select_lan.select_by_visible_text(language)
+        self.driver.find_element(By.ID, "login-button").click()
+        assert_that(expected_title).is_equal_to(self.driver.title)
+
+    def test_invalid_login(self):
+        self.driver.find_element(By.ID, "authUser").send_keys("john")
+        self.driver.find_element(By.CSS_SELECTOR, "#clearPass").send_keys("john123")
+        select_lan = Select(self.driver.find_element(By.XPATH, "//select[@name='languageChoice']"))
+        select_lan.select_by_visible_text("Dutch")
+        self.driver.find_element(By.ID, "login-button").click()
+        # assert the error message - Invalid username or password
---
+++ utilities/data_source.py
@@ -0,0 +1,8 @@
+""" test data for valid login test """
+test_valid_login_data = [
+    ("admin", "pass", "English (Indian)", "OpenEMR"),
+    ("physician", "physician", "English (Indian)", "OpenEMR"),
+    ("accountant", "accountant", "English (Indian)", "OpenEMR")
+]
+
+
