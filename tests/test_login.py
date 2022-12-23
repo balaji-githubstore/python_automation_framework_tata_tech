fcef7d208f3001dc01c53090df2248de593575f9
balaji-githubstore <balaji0017@gmail.com>
2022-12-23 12:48:42 +0530

    Code as on dec 23

--- tests/test_login.py
+++ tests/test_login.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.select import Select
 
 from base.webdriver_listener import WebDriverWrapper
 from utilities import data_source, read_data
-
+#dec 23 code
 
 class TestLoginUI(WebDriverWrapper):
     def test_title(self):
